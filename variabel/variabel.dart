41e72370d05d9819680847a885b25a04615230b4
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-01-31 00:41:36 +0700

    first commit

---
+++ variabel/variabel.dart
@@ -0,0 +1,14 @@
+void main() {
+  var name = 'Voyager I';
+  var year = 1977;
+  var height = 3.7;
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  var image = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg'
+  };
+
+  print("nama $name, lahir $year, tinggi $height");
+  print(flybyObjects);
+  print(image);
+}
